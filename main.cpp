1e28854733f7259a3cfa2ed23ff52e5237e8f0e0
Chili <planetchili@users.noreply.github.com>
2022-10-01 18:13:28 +0900

    end of unbalance prep 2

--- main.cpp
+++ main.cpp
@@ -9,49 +9,94 @@
 #include <span>
 #include <algorithm>
 #include <numeric>
+#include <numbers>
+#include <fstream>
+#include <format>
+#include <functional>
 #include "ChiliTimer.h"
 
 // experimental settings
 constexpr size_t WorkerCount = 4;
-constexpr size_t ChunkSize = 100;
+constexpr size_t ChunkSize = 8'000;
 constexpr size_t ChunkCount = 100;
 constexpr size_t SubsetSize = ChunkSize / WorkerCount;
 constexpr size_t LightIterations = 100;
 constexpr size_t HeavyIterations = 1'000;
-constexpr double ProbabilityHeavy = .02;
+constexpr double ProbabilityHeavy = .15;
 
 static_assert(ChunkSize >= WorkerCount);
 static_assert(ChunkSize % WorkerCount == 0);
 
 struct Task
 {
-    unsigned int val;
+    double val;
     bool heavy;
     unsigned int Process() const
     {
         const auto iterations = heavy ? HeavyIterations : LightIterations;
-        double intermediate = 2. * (double(val) / double(std::numeric_limits<unsigned int>::max())) - 1.;
+        auto intermediate = val;
         for (size_t i = 0; i < iterations; i++)
         {
-            intermediate = std::sin(std::cos(intermediate));
+            const auto digits = unsigned int(std::abs(std::sin(std::cos(intermediate)) * 10'000'000.)) % 100'000;
+            intermediate = double(digits) / 10'000.;
         }
-        return unsigned int((intermediate + 1.) / 2. * double(std::numeric_limits<unsigned int>::max()));
+        return unsigned int(std::exp(intermediate));
     }
 };
 
-std::vector<std::array<Task, ChunkSize>> GenerateDatasets()
+struct ChunkTimingInfo
+{
+    std::array<float, WorkerCount> timeSpentWorkingPerThread;
+    std::array<size_t, WorkerCount> numberOfHeavyItemsPerThread;
+    float totalChunkTime;
+};
+
+std::vector<std::array<Task, ChunkSize>> GenerateDatasetRandom()
 {
     std::minstd_rand rne;
     std::bernoulli_distribution hDist{ ProbabilityHeavy };
+    std::uniform_real_distribution rDist{ 0., std::numbers::pi };
 
     std::vector<std::array<Task, ChunkSize>> chunks(ChunkCount);
 
     for (auto& chunk : chunks)
     {
-        std::ranges::generate(chunk, [&] { return Task{ .val = rne(), .heavy = hDist(rne)}; });
+        std::ranges::generate(chunk, [&] { return Task{ .val = rDist(rne), .heavy = hDist(rne)}; });
     }
 
     return chunks;
+}
+
+std::vector<std::array<Task, ChunkSize>> GenerateDatasetEvenly()
+{
+    std::minstd_rand rne;
+    std::uniform_real_distribution rDist{ 0., std::numbers::pi };
+
+    const auto everyNth = int(1. / ProbabilityHeavy);
+
+    std::vector<std::array<Task, ChunkSize>> chunks(ChunkCount);
+
+    for (auto& chunk : chunks)
+    {
+        std::ranges::generate(chunk, [&, i = 0]() mutable {
+            const auto isHeavy = i++ % everyNth == 0;
+            return Task{ .val = rDist(rne), .heavy = isHeavy };
+        });
+    }
+
+    return chunks;
+}
+
+std::vector<std::array<Task, ChunkSize>> GenerateDatasetStacked()
+{
+    auto data = GenerateDatasetEvenly();
+
+    for (auto& chunk : data)
+    {
+        std::ranges::partition(chunk, std::identity{}, &Task::heavy);
+    }
+
+    return data;
 }
 
 class MasterControl
@@ -115,12 +160,26 @@ public:
     {
         return accumulation;
     }
+    float GetJobWorkTime() const
+    {
+        return workTime;
+    }
+    size_t GetNumHeavyItemsProcessed() const
+    {
+        return numHeavyItemsProcessed;
+    }
+    ~Worker()
+    {
+        Kill();
+    }
 private:
     void ProcessData_()
     {
+        numHeavyItemsProcessed = 0;
         for (const auto& task : input)
         {
             accumulation += task.Process();
+            numHeavyItemsProcessed += task.heavy ? 1 : 0;
         }
     }
     void Run_()
@@ -128,12 +187,17 @@ private:
         std::unique_lock lk{ mtx };
         while (true)
         {
+            ChiliTimer timer;
             cv.wait(lk, [this] {return !input.empty() || dying; });
             if (dying)
             {
                 break;
             }
+
+            timer.Mark();
             ProcessData_();
+            workTime = timer.Peek();
+
             input = {};
             pMaster->SignalDone();
         }
@@ -146,14 +210,23 @@ private:
     std::span<const Task> input;
     unsigned int accumulation = 0;
     bool dying = false;
+    float workTime = -1.f;
+    size_t numHeavyItemsProcessed;
 };
 
-int DoExperiment()
+int DoExperiment(bool stacked)
 {
-    const auto chunks = GenerateDatasets();
+    const auto chunks = [=] {
+        if (stacked) {
+            return GenerateDatasetStacked();
+        }
+        else {
+            return GenerateDatasetEvenly();
+        }
+    }();
 
-    ChiliTimer timer;
-    timer.Mark();
+    ChiliTimer totalTimer;
+    totalTimer.Mark();
 
     MasterControl mctrl;
 
@@ -163,16 +236,30 @@ int DoExperiment()
         workerPtrs.push_back(std::make_unique<Worker>(&mctrl));
     }
 
+    std::vector<ChunkTimingInfo> timings;
+    timings.reserve(ChunkCount);
+
+    ChiliTimer chunkTimer;
     for (const auto& chunk : chunks)
     {
+        chunkTimer.Mark();
         for (size_t iSubset = 0; iSubset < WorkerCount; iSubset++)
         {
             workerPtrs[iSubset]->SetJob(std::span{ &chunk[iSubset * SubsetSize], SubsetSize });
         }
         mctrl.WaitForAllDone();
-    }
-    const auto t = timer.Peek();
+        const auto chunkTime = chunkTimer.Peek();
 
+        timings.push_back({});
+        for (size_t i = 0; i < WorkerCount; i++)
+        {
+            timings.back().numberOfHeavyItemsPerThread[i] = workerPtrs[i]->GetNumHeavyItemsProcessed();
+            timings.back().timeSpentWorkingPerThread[i] = workerPtrs[i]->GetJobWorkTime();
+        }
+        timings.back().totalChunkTime = chunkTime;
+    }
+    const auto t = totalTimer.Peek();
+    
     std::cout << "Processing took " << t << " seconds\n";
     unsigned int finalResult = 0;
     for (const auto& w : workerPtrs)
@@ -181,9 +268,28 @@ int DoExperiment()
     }
     std::cout << "Result is " << finalResult << std::endl;
 
-    for (auto& w : workerPtrs)
+    // output csv of chunk timings
+    // worktime, idletime, numberofheavies x workers + total time, total heavies
+    std::ofstream csv{ "timings.csv", std::ios_base::trunc };
+    for (size_t i = 0; i < WorkerCount; i++)
     {
-        w->Kill();
+        csv << std::format("work_{0:},idle_{0:},heavy_{0:},", i);
+    }
+    csv << "chunktime,total_idle,total_heavy\n";
+
+    for (const auto& chunk : timings)
+    {
+        float totalIdle = 0.f;
+        size_t totalHeavy = 0;
+        for (size_t i = 0; i < WorkerCount; i++)
+        {
+            const auto idle = chunk.totalChunkTime - chunk.timeSpentWorkingPerThread[i];
+            const auto heavy = chunk.numberOfHeavyItemsPerThread[i];
+            csv << std::format("{},{},{},", chunk.timeSpentWorkingPerThread[i], idle, heavy);
+            totalIdle += idle;
+            totalHeavy += heavy;
+        }
+        csv << std::format("{},{},{}\n", chunk.totalChunkTime, totalIdle, totalHeavy);
     }
 
     return 0;
@@ -191,5 +297,10 @@ int DoExperiment()
 
 int main(int argc, char** argv)
 {
-    return DoExperiment();
+    using namespace std::string_literals;
+    bool stacked = false;
+    if (argc > 1 && argv[1] == "--stacked"s) {
+        stacked = true;
+    }
+    return DoExperiment(stacked);
 }
