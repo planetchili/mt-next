f208b297d564cc7f73b7651901d99f2f7163b458
Chili <planetchili@users.noreply.github.com>
2022-10-24 17:13:08 +0900

    more tasks, lighter tasks

--- Constants.h
+++ Constants.h
@@ -2,10 +2,10 @@
 
 inline constexpr bool ChunkMeasurementEnabled = false;
 inline constexpr size_t WorkerCount = 4;
-inline constexpr size_t ChunkSize = 8'000;
-inline constexpr size_t ChunkCount = 100;
-inline constexpr size_t LightIterations = 100;
-inline constexpr size_t HeavyIterations = 1'000;
+inline constexpr size_t ChunkSize = 16'000;
+inline constexpr size_t ChunkCount = 1000;
+inline constexpr size_t LightIterations = 2;
+inline constexpr size_t HeavyIterations = 20;
 inline constexpr double ProbabilityHeavy = .15;
 
 inline constexpr size_t SubsetSize = ChunkSize / WorkerCount;
